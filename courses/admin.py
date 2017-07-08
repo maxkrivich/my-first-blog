6a06324b04ff2c072642df170d9a2ed0ffaf78b4
Max Krivich <maxkrivich@gmail.com>
2017-07-08 16:14:54 +0300

    task3 admin model done

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Course, Lesson
 
+class LessonInline(admin.TabularInline):
+	model = Lesson
+	extra = 3
+
 admin.site.register(Lesson)
-admin.site.register(Course)
+
+class CourseAdmin(admin.ModelAdmin):
+	search_fields = ['name']
+	list_display = ('name', 'short_description')
+	inlines = [LessonInline]
+
+admin.site.register(Course, CourseAdmin)
